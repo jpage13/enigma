ec1c270300ea359a3fc4995e2c25e41b7a5a4089
Jimmy Page <jpage@knights.ucf.edu>
2022-01-15 16:19:30 -0700

    test: it exists

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,3 +3,10 @@ require './spec/spec_helper'
 require './lib/key'
 require './lib/offset'
 require './lib/shift'
+
+describe Enigma do
+  it 'exists' do
+    enigma = Enigma.new
+    expect(enigma).to be_instance_of(Enigma)
+  end
+end
