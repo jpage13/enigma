98af188848011144e33d9fd1e7bd084bb60ce6ef
Jimmy Page <jpage@knights.ucf.edu>
2022-01-16 13:06:34 -0700

    made tests more dynamic

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -8,14 +8,14 @@ describe Shift do
   it 'exists' do
     key = Key.new("79671")
     offset = Offset.new("150122")
-    shift = Shift.new(key, offset)
+    shift = Shift.new(key, date)
     expect(shift).to be_instance_of(Shift)
   end
 
   it 'can give each shift value' do
     key = Key.new("79671")
     offset = Offset.new("150122")
-    shift = Shift.new(key, offset)
+    shift = Shift.new(key, date)
     expect(shift.a_shift).to eq(83)
     expect(shift.b_shift).to eq(104)
     expect(shift.c_shift).to eq(75)
