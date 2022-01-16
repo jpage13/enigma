5ee9c90153d6a7258759bd1a7b7984de8002b58b
Jimmy Page <jpage@knights.ucf.edu>
2022-01-16 14:25:15 -0700

    refactored initialize

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,25 +2,26 @@ require './lib/key'
 require './lib/offset'
 
 class Shift
+  attr_reader :key, :offset
 
-  def initialize(key, date)
-    @key = Key.new(key)
-    @offset = Offset.new(date)
+  def initialize(key = Key.new, date = Offset.new)
+    @key = key
+    @offset = date
   end
 
   def a_shift
-    @key.a_key.to_i + @offset.a_offset.to_i
+    self.key.a_key.to_i + self.offset.a_offset.to_i
   end
 
   def b_shift
-    @key.b_key.to_i + @offset.b_offset.to_i
+    self.key.b_key.to_i + self.offset.b_offset.to_i
   end
 
   def c_shift
-    @key.c_key.to_i + @offset.c_offset.to_i
+    self.key.c_key.to_i + self.offset.c_offset.to_i
   end
 
   def d_shift
-    @key.d_key.to_i + @offset.d_offset.to_i
+    self.key.d_key.to_i + self.offset.d_offset.to_i
   end
 end
