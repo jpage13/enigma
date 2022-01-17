8dee199fb2c2d15996473ad7bd5c9699b4682581
Jimmy Page <jpage@knights.ucf.edu>
2022-01-16 18:53:07 -0700

    finished encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,14 +8,16 @@ class Enigma
 
   def initialize
     @alphabet = ("a".."z").to_a << " "
+    @key = Key.new.to_s
+    @date = Offset.new.to_s
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = @key, date = @date)
     shifts = Shift.new(key, date)
     encryption = {}
     encrypted_message = []
-    parse_message = message.chars.each_slice(4).to_a
-    parse_message.each do |group|
+    message_chunk = message.chars.each_slice(4).to_a
+    message_chunk.each do |group|
       group_hash = Hash[group.map.with_index {|letter, index| [index, letter]}]
       group_hash.each do |index, letter|
         if index == 0
@@ -36,7 +38,28 @@ class Enigma
     encryption
   end
 
-  def decrypt(ciphertext, key, date)
+  def decrypt(ciphertext, key = @key, date = @date)
+    shifts = Shift.new(key, date)
+    decryption = {}
+    decrypted_message = []
+    ciphertext_chunks = ciphertext.chars.each_slice(4).to_a
+    ciphertext_chunks.each do |group|
+      group_hash = Hash[group.map.with_index {|letter, index| [index, letter]}]
+      group_hash.each do |index, letter|
+        if index == 0
+          decrypted_message << @alphabet.rotate(shifts.a_shift*-1)[@alphabet.index(group[0].downcase)]
+        elsif index == 1
+          decrypted_message << @alphabet.rotate(shifts.b_shift*-1)[@alphabet.index(group[1].downcase)]
+       elsif index == 2
+         decrypted_message << @alphabet.rotate(shifts.c_shift*-1)[@alphabet.index(group[2].downcase)]
+       elsif index == 3
+         decrypted_message << @alphabet.rotate(shifts.d_shift*-1)[@alphabet.index(group[3].downcase)]
+       end
+     end
+   end
+   decryption[:decryption] = decrypted_message.join
+   decryption[:key] = key
+   decryption[:date] = date
+   decryption
   end
-
 end
