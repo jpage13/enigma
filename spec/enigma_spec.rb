3112f336d7e54c9bc8e093f4e22cd2aeff34b900
Jimmy Page <jpage@knights.ucf.edu>
2022-01-16 17:23:13 -0700

    created tests for enigma

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,14 +1,33 @@
 require 'rspec'
 require './spec/spec_helper'
-require 'date'
-require './lib/key'
-require './lib/offset'
-require './lib/shift'
 require './lib/enigma'
 
 describe Enigma do
-  it 'exists' do
-    enigma = Enigma.new
-    expect(enigma).to be_instance_of(Enigma)
+  before :each do
+    @enigma = Enigma.new
   end
+
+  it 'exists' do
+    expect(@enigma).to be_instance_of(Enigma)
+  end
+
+  it 'can encrypt message with given key and date' do
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it 'can decrypt ciphertext with given key and date' do
+    expected =  {decryption: "hello world", key: "02715", date: "040895"}
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+  end
+
+  xit 'can encrypt a message with a key and no date argument' do
+  end
+
+  xit 'can decrypt a message with a key and no date argument' do
+  end
+
+  xit 'can encypt a message with no key or date arguments' do
+  end
+
 end
