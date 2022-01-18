a123eb98ca10c104e5c0d97b496f772339510c80
Jimmy Page <jpage@knights.ucf.edu>
2022-01-17 17:07:38 -0700

    fixed bug and argument names in encrypt and decrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,17 +1,18 @@
 require 'date'
 require './lib/enigma'
-require './lib/fileable'
+require './lib/file_accessor'
 
 include FileAccessor
 
-message = reader(ARGV[0])
-enigma = Enigma.new
-secret_encryption = enigma.encrypt(secret)
+secret_message = reader(ARGV[0])
 
-encryption = secret_encryption[:encryption]
+enigma = Enigma.new
+secret_encryption = enigma.encrypt(secret_message)
+
+cryption = secret_encryption[:encryption]
 key = secret_encryption[:key]
 date = secret_encryption[:date]
 
-writer(encryption, ARGV[1])
+writer(cryption, ARGV[1])
 
 puts "Created 'encrypted.txt' with the key #{key} and date #{date}"
